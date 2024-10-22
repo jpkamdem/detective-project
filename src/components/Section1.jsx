67c0a44fbda31ae963ea204e96cf818e7f18e820
jkam <jpkamdem@proton.me>
2024-10-22 17:50:28 +0200

    ajout synopsis

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -5,7 +5,26 @@ export default function Section1({ onClick }) {
   return (
     <>
       <div id="section1" className={styles.container}>
-        <h2>Section 1 : Enquêtez sur la citrouille</h2>
+        <h2 className={styles.headline}>Synopsis :</h2>
+        <p className={styles.text}>
+          Après plusieurs années sans se revoir, un groupe d'amis d'enfance
+          décide de se réunir pour un week-end dans un chalet isolé au bord d'un
+          lac, un endroit chargé de souvenirs de leur jeunesse. Le lac,
+          autrefois un lieu de bonheur et d'insouciance, semble désormais
+          imprégné d'une atmosphère oppressante et mystérieuse. Alors que les
+          retrouvailles commencent joyeusement, des événements étranges et
+          inexplicables viennent troubler leur tranquilité. Lecalme paisible du
+          lac est rompu par des murmures étouffés qui semblent provenir des
+          profondeurs. Des objets disparaissent, des voix chuchotent dans le
+          vent, et des rêves troublant hantent les nuits de chacun. Petit à
+          petit, les tensions montent et des secrets enfouis depuis longtemps
+          resurgissent. Ce qui semblait n'être qu'un week-end nostalgique se
+          transforme en un véritable cauchemar, alors que les amis commencent à
+          se demander si quelque chose, ou quelqu'un, n'est pas en train de les
+          observer depuis les eaux sombres. Au fur et à mesure que la réalité se
+          brouille, les murmures du lac révèlent une vérité bien plus
+          terrifiante que quiconque aurait pu l'imaginer...
+        </p>
         <ScrollButton
           label="Section numéro TWO"
           target="section2"
