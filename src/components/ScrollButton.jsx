14bd87a1c1afa4c51e220db0627500e1387142d6
jkam <jpkamdem@proton.me>
2024-10-27 01:34:08 +0200

    j'avais oublié le scrollbutton

--- src/components/ScrollButton.jsx
+++ src/components/ScrollButton.jsx
@@ -5,7 +5,7 @@ import { PiHouseLineLight } from "react-icons/pi";
 export default function ScrollButton({ target, onClick }) {
   return (
     <>
-      <Link to={target} smooth={true} duration={1} onClick={onClick}>
+      <Link to={target} smooth={true} duration={5500} onClick={onClick}>
         {/* duration: 5500 */}
         <PiHouseLineLight size={70} className={styles.btn} />
       </Link>
