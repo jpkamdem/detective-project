7941821131644c5ae5251754265d76c1e654fb8a
jkam <jpkamdem@proton.me>
2024-10-27 09:15:40 +0100

    dénouement, fin ?

--- src/components/Section13.jsx
+++ src/components/Section13.jsx
@@ -7,7 +7,20 @@ export default function Section8({ onClick }) {
     <>
       <div id="section13" className={styles.container}>
         <Header label="Dénouement" />
-        <p className={styles.text}>Lorem ipsum dolor sit amet.</p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet. Après avoir résolu une série d’énigmes
+          disséminées dans le vieux chalet, le groupe d’amis sent une force
+          mystérieuse qui les attire vers le lac. Alors qu’ils approchent, le
+          calme de la nuit devient oppressant. L’air devient lourd, et des voix
+          à peine audibles émergent de l’obscurité. Les murmures se transforment
+          en cris chuchotés, se mêlant au vent qui souffle de plus en plus fort,
+          tandis que l'eau du lac s'agite étrangement. Leurs pensées
+          s’éclaircissent enfin :
+          <strong>tous ces mots ne sont pas destinés à être utilisés.</strong>
+          <br />
+          "Quand ______ tombe, le ______ devient ______. Dans le silence de la
+          ______, la ______ demeure."
+        </p>
         <ScrollButton onClick={onClick} target="section14" />
       </div>
     </>
